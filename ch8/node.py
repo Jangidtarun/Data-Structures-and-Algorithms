b419f5bb60472f5101e16c644422d882e5ea0c0a
Jangidtarun <tarundjangid@gmail.com>
2025-05-04 10:33:14 +0530

    removed unnecessary head node definition; added a count field; implemented the insert logic again

--- ch8/node.py
+++ ch8/node.py
@@ -1,50 +1,63 @@
 # Node class
 class Node:
-    def __init__(self, data, bal=0, left=None, right=None):
+    LH = -1
+    EH =  0
+    RH = +1
+
+    def __init__(self, data, count=0, bal=0, left=None, right=None):
+        self.count = count
         self.data = data
         self.bal = bal
         self.left = left
         self.right = right
 
-    
-    def left_balance(self):
-        if self.bal < 0:
-            self.rotate_right()
+
+    def insert(self, newdata, taller):
+        if self is None:
+            self = Node(newdata)
+            taller = True
         else:
-            self.left.rotate_left()
-            self.rotate_right()
+            if newdata < self.data:
+                self.left.insert(newdata, taller)
+                if taller:
+                    if root.bal == LH:
+                        # was left high
+                        # rotate
+                        self.ins_left_bal(taller)
+                    elif self.bal == EH:
+                        # was balance
+                        # now left high
+                        self.bal = LH
+                    elif self.bal == RH:
+                        # was right high
+                        # now balanced
+                        self.bal == EH
+                        taller = False
+            else:
+                self.right.insert(newdata, taller)
+                if taller:
+                    if self.bal == LH:
+                        # was lef high
+                        # now balanced
+                        self.bal = EH
+                        taller = False
+                    elif self.bal == EH:
+                        # was balanced
+                        # now right high
+                        self.bal = RH
+                    elif self.bal == RH:
+                        # was right high
+                        # rotate
+                        self.ins_right_bal(taller)
 
 
-    def right_balance(self):
-        if self.bal > 0:
-            self.rotate_left()
-        else:
-            self.right.rotate_right()
-            self.rotate_left()
-    
-
-    def rotate_right(self):
-        newroot = self.left
-        tmp = newroot.right
-        newroot.right = self
-        self.left = tmp
-        return newroot
 
 
-    def rotate_left(self):
-        newroot = self.right
-        tmp = newroot.left
-        newroot.left = self
-        self.right = tmp
-        return newroot
 
-    
-    def preorder(self):
-        print(self.data, end=" ")
-        if self.left:
-            self.left.preorder()
-        if self.right:
-            self.right.preorder()
+
+
+
+
 
 if __name__ == "__main__":
     pass
