689bed5c86db71e2083368becbd7e4c37bc71d81
Tarun Jangid <tarundjangid@gmail.com>
2025-05-11 20:31:18 +0530

    finally good god; algorithms

---
+++ ch12/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+void selection_sort(int arr[], int size) {
+	if (size == 0)
+		return;
+
+	int first_unsorted_loc = 0;
+
+	while (first_unsorted_loc + 1 < size) {
+		int min_val_loc = first_unsorted_loc;
+		for (int i = first_unsorted_loc; i < size; i++) {
+			if (arr[min_val_loc] > arr[i]) {
+				min_val_loc = i;
+			}
+		}
+
+		int tmp = arr[min_val_loc];
+		arr[min_val_loc] = arr[first_unsorted_loc];
+		arr[first_unsorted_loc] = tmp;
+
+		first_unsorted_loc += 1;
+	}
+}
+
+
+void print_arr(int arr[], int size) {
+	for (int i = 0; i < size; i++) {
+		printf("%2d ", arr[i]);
+	}
+	printf("\n");
+}
+
+
+void reheap(int arr[], int size) {
+
+}
+
+
+int main() {
+	int arr[] = {93, 74, 90, 37, 57, 61, 30, 14, 19, 56, 24};
+	int size = sizeof(arr) / sizeof(int);
+
+	print_arr(arr, size);
+	selection_sort(arr, size);
+	print_arr(arr, size);
+
+	return 0;
+}
