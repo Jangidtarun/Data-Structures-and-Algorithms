de54bc110935412436109aea449fa37c82fd0cfd
Tarun Jangid <tarundjangid@gmail.com>
2025-05-11 11:45:25 +0530

    implement function for inserting and deleting arc

--- ch11/p11-graph-adt.h
+++ ch11/p11-graph-adt.h
@@ -107,4 +107,91 @@ int graph_delete_vertex(struct graph *g, void *dltptr) {
 }
 
 
+int graph_insert_arc(struct graph *g, void *from, void *to) {
+	struct arc *newptr = malloc(sizeof(struct arc));
+	if (!newptr) {
+		return -1;
+	}
+
+	struct vertex *vert_from = g->first;
+	while (vert_from && (g->cmp(from, vert_from->dataptr) > 0)) {
+		vert_from = vert_from->pnext_vertex;
+	}
+
+	if (!vert_from || (g->cmp(from, vert_from->dataptr) != 0))
+		return -2;
+
+	struct vertex *vert_to = g->first;
+	while (vert_to && (g->cmp(to, vert_to->dataptr) > 0)) {
+		vert_to = vert_to->pnext_vertex;
+	}
+
+	if (!vert_to || (g->cmp(to, vert_to->dataptr) != ))
+		return -3;
+
+	vert_from->outdeg += 1;
+	vert_to->indeg += 1;
+	newptr->destination = vert_to;
+	if (!vert_from->parc) {
+		vert_from->parc = newptr;
+		newptr->pnext_arc = NULL;
+		return 1;
+	}
+
+	struct arc *arc_predptr = NULL;
+	struct arc *arc_walkptr = vert_from->parc;
+	while (arc_walkptr && 
+			(g->cmp(to, arc_walkptr->destination->dataptr) >= 0)) {
+		arc_predptr = arc_walkptr;
+		arc_walkptr = arc_walkptr->pnext_arc;
+	}
+
+	if (!arc_predptr)
+		vert_from->parc = newptr;
+	else
+		arc_predptr->pnext_arc = newptr;
+
+	newptr->pnext_arc = arc_walkptr;
+	return 1;
+}
+
+
+int graph_delete_arc(struct graph *g, void *from, void *to) {
+	if (!g->first)
+		return -2;
+
+	struct vertex *vert_from = g->first;
+	while (vert_from && (g->cmp(from, vert_from->dataptr) > 0))
+		vert_from = vert_from->pnext_vertex;
+
+	if (!vert_from || g->cmp(from, vert_from->dataptr) != 0)
+		return -2;
+
+	if (!vert_from->parc)
+		return -3;
+
+	struct arc *arc_preptr = NULL;
+	struct arc *arc_walkptr = vert_from->parc;
+	while (arc_walkptr && g->cmp(to, arc_walkptr->destination->dataptr) > 0) {
+		arc_preptr = arc_walkptr;
+		arc_walkptr = arc_walkptr->pnext_arc;
+	}
+
+	if (!arc_walkptr || g->cmp(to, arc_walkptr->destination->dataptr) != 0)
+		return -3;
+
+	struct vertex *vert_to = arc_walkptr->destination;
+
+	vert_from->outdeg -= 1;
+	vert_to->indeg -= 1;
+	if (!arc_preptr)
+		vert_from->parc = arc_walkptr->pnext_arc;
+	else
+		arc_preptr->pnext_arc = arc_walkptr->pnext_arc;
+
+	free(arc_walkptr);
+	return 1;
+}
+
+
 #endif
