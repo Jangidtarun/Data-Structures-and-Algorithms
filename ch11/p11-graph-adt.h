2b3aaa13a381dfc797984c49b6cc6d1599d24c46
Tarun Jangid <tarundjangid@gmail.com>
2025-05-11 11:04:49 +0530

    implement graph_insert_vertex function

--- ch11/p11-graph-adt.h
+++ ch11/p11-graph-adt.h
@@ -7,31 +7,31 @@
 #include "../ch3/p3-stack-adt.h"
 
 
-struct VERTEX {
-	struct VERTEX *pnext_vertex;
+struct vertex {
+	struct vertex *pnext_vertex;
 	void *dataptr;
-	int indegree;
-	int outdegree;
+	int indeg;
+	int outdeg;
 	bool processed;
-	struct ARC *parc;
+	struct arc *parc;
 };
 
 
-struct GRAPH {
+struct graph {
 	int count;
 	struct vertex *first;
 	int (*cmp) (void *arg1, void *arg2);
 };
 
 
-struct ARC {
+struct arc {
 	struct vertex *destination;
-	struct ARC *pnextarc;
+	struct arc *pnext_arc;
 };
 
 
-struct GRAPH *graph_init(int (*cmp) (void *arg1, void *arg2)) {
-	struct GRAPH *g = malloc(sizeof(struct GRAPH));
+struct graph *graph_init(int (*cmp) (void *arg1, void *arg2)) {
+	struct graph *g = malloc(sizeof(struct graph));
 	if (g) {
 		g->count = 0;
 		g->first = NULL;
@@ -39,6 +39,42 @@ struct GRAPH *graph_init(int (*cmp) (void *arg1, void *arg2)) {
 	}
 
 	return g;
+}
+
+
+bool graph_insert_vertex(struct graph *g, void *dataptr) {
+	struct vertex *newptr = malloc(sizeof(struct vertex));
+	if (!newptr) {
+		return false;
+	}
+
+	newptr->pnext_vertex = NULL;
+	newptr->dataptr = dataptr;
+	newptr->indeg = 0;
+	newptr->outdeg = 0;
+	newptr->processed = false;
+	newptr->parc = NULL;
+
+	if (!g->first)
+		g->first = newptr;
+	else {
+		struct vertex *predptr = NULL;
+		struct vertex *locptr = g->first;
+		while (locptr && (g->cmp(dataptr, locptr->dataptr) > 0)) {
+			predptr = locptr;
+			locptr = locptr->pnext_vertex;
+		}
+
+		if (!predptr) {
+			g->first = newptr;
+		} else {
+			predptr->pnext_vertex = newptr;
+		}
+
+		newptr->pnext_vertex = locptr;
+	}
+
+	return true;
 }
 
 
